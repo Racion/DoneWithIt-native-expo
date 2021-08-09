b4790e58447aff730f2b3f07d7d67d76369faa05
Rafael H. Osorio <rafa.osorioa@gmail.com>
2021-08-09 18:30:47 -0500

    First commit

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  let x = 1;
+  console.log("App Executed");
+
+  //function component
   return (
+    //view works as a div
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello React Native</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+//Create the stylesheet object, we pass an object that is container, this is not css is js property
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
